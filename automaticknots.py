5c037b4a566ee0dd4d203ecfe4eaf0d3979c0b9e
Henrique Vaz de Araújo <143658602+Henrivaz@users.noreply.github.com>
2025-03-20 16:31:59 -0300

    Add files via upload
    added txt file reading

--- automaticknots.py
+++ automaticknots.py
@@ -1,22 +1,28 @@
-import numpy as np  
+import numpy as np
+
+def carregar_pontos_de_arquivo(arquivo):
+    # Carrega os dados do arquivo de texto
+    Q = np.loadtxt(arquivo, delimiter=',')
+    return Q
 
 def automaticknots(Q, espaco_entre_knots = 1.0):  
-    
-    num_pontos = Q.shape[0]  # Número de linhas da matriz (quantidade de pontos)
-    distancia_total = 0   #começa em 0 pq ainda nao foi percorrida
+    num_pontos = len(Q)  # Número de pontos (linhas da matriz)
+    distancia_total = 0   # Começa em 0 porque ainda não foi percorrida
 
-    # distancia euclidiana entre os pontos
+    # Distância euclidiana entre os pontos
     for i in range(num_pontos - 1):  
         p1, p2 = Q[i], Q[i + 1]  
         distancia_total += np.linalg.norm(p2 - p1)  
 
-    # Calcula o número de nós baseada na distancia
-    n = int(distancia_total / espaco_entre_knots)  
+    # Calcula o número de nós baseado na distância total
+    n = int(distancia_total / espaco_entre_knots)
+    
+    print("Número de nós calculado:", n)
+    return n
 
-    return n  
-
-# Q = np.linspace([0, 0], [10, 0], num=65)    #pega os 65 pontos dentro da distancia definida (exemplo: 10 m)
-# espaco_entre_nos = 1.5                     # Espaçamento entre os nós (exemplo: 5 cm)
-# n_calculado = automaticknots(Q, espaco_entre_nos)
-
-# print("Número de nós calculado:", n_calculado)
+# Exemplo de uso da função
+arquivo = "C:/Users/henri/OneDrive/Desktop/PiecewiseG1BezierFit-main/scripts/teleop_data.txt"  # Nome do arquivo .txt com os pontos
+Q = carregar_pontos_de_arquivo(arquivo)
+espaco_entre_knots = 5  # Defina o valor de espaçamento entre knots
+n = automaticknots(Q, espaco_entre_knots)
+print("Número final de nós:", n)
